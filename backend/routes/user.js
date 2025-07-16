5b20e4563b1a5215ec072af0e1bd2ae94f813284
sunny singh <sunny2005sin@gmail.com>
2025-07-16 13:20:17 +0530

    user signup/signin updated

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,18 +1,52 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const userRouter = Router();
+const { userModel } = require("../db");
+const jwt = require("jsonwebtoken");
+const { JWT_USER_PASSWORD } = require("../config")
 
 
 
-userRouter.post("/signup", function (req, res) {
+userRouter.post("/signup",async function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+//zod validation
+
+    try {
+        await userModel.create({
+            email,
+            password,
+            firstName,
+            lastName
+        })
+    } catch (e) {
+        res.status(403).json({
+            message: "user you are not signed up"
+        })
+    }
     res.json({
-        message: "you are signed up"
+        message: "user you are signed up"
     })
 })
 
-userRouter.post("/signin", function (req, res) {
-    res.json({
-        message: "you are signed in"
+userRouter.post("/signin", async function (req, res) {
+    const {email, password}=req.body;
+    const user= await userModel.findOne({
+        email: email,
+        password: password    //password encrpt needed
     })
+    if(user){
+        const token = jwt.sign({
+         //user id
+         id: user._id,
+        }, JWT_USER_PASSWORD);
+        
+        res.json({
+            token: token
+        })
+    }
+   
 })
 userRouter.get("/purchases", function (req, res) {
     res.json({
@@ -20,6 +54,6 @@ userRouter.get("/purchases", function (req, res) {
     })
 });
 
-module.exports={
+module.exports = {
     userRouter: userRouter
 }
