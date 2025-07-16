66e242fd85ffa402edc04702927d249380258f83
sunny singh <sunny2005sin@gmail.com>
2025-07-16 13:35:54 +0530

    config error solved

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
-const JWT_ADMIN_PASSWORD = "sunnysingh";
-const JWT_USER_PASSWORD = "usersunny"
+const JWT_ADMIN_PASSWORD = process.env.JWT_ADMIN_PASSWORD;
+const JWT_USER_PASSWORD =  process.env.JWT_USER_PASSWORD;
 
 module.exports={
     JWT_ADMIN_PASSWORD,
